1209d849d34a68e5d97900e32e650d2fe73a316d
nksazonov <nsazonov@openware.com>
2022-09-01 15:36:17 +0300

    clearify example benchmark

--- benchmarks/bench.spec.ts
+++ benchmarks/bench.spec.ts
@@ -2,14 +2,29 @@
 import {expect} from 'chai';
 import {ethers} from 'hardhat';
 
+import './setup';
 import {gasUsed} from './helpers';
 
 describe('benchmark test', () => {
-  it('blah', async () => {
+  it('Deployment', async () => {
     const tokenFactory = await ethers.getContractFactory('SimpleERC20');
     const token = await tokenFactory.deploy('Yellow', 'YLW', 16);
     await token.deployed();
     await expect(token.deployTransaction).to.consumeGas(2224727);
-    await expect(await gasUsed(token.deployTransaction)).to.equalGas(2224727);
+  });
+  it('Deposit', async () => {
+    const [user, someone] = await ethers.getSigners();
+
+    // start setup
+    const tokenFactory = await ethers.getContractFactory('SimpleERC20');
+    const token = await tokenFactory.deploy('Yellow', 'YLW', 16);
+    await token.deployed();
+
+    await token.mintTo(user.address, 1000);
+    // end setup
+
+    await expect(
+      await gasUsed(await token.connect(user).transfer(someone.address, 15))
+    ).to.equalGas(15000);
   });
 });
