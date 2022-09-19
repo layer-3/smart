b2935b0131219bc8ccc94a6e66c7d71afbcadc10
lckmidnight <lckmidnight@gmail.com>
2022-09-19 11:10:39 +0200

    format VaultImpl.sol

--- contracts/yellow/VaultImpl.sol
+++ contracts/yellow/VaultImpl.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.16;
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
 import './VaultImplBase.sol';
 import './IVault.sol';
 
@@ -12,8 +13,6 @@ import './IVault.sol';
  */
 contract VaultImpl is VaultImplBase, IVault {
     using Counters for Counters.Counter;
-
-    bool private isSetup = false;
 
     /**
      * Deposit type identifier value.
@@ -25,6 +24,8 @@ contract VaultImpl is VaultImplBase, IVault {
      */
     bytes32 public constant WITHDRAW_ACTION = keccak256('YELLOW_VAULT_WITHDRAW_ACTION');
 
+    bool private _isSetup = false;
+
     // Not a real address, only public key exists.
     address private _brokerVirtualAddress;
     // Not a real address, only public key exists.
@@ -34,197 +35,6 @@ contract VaultImpl is VaultImplBase, IVault {
 
     // Keep track of used signatures to prevent reuse before expiration.
     mapping(address => mapping(bytes32 => bool)) private _sigUsage;
-
-    /**
-     * @notice The setup function sets virtual addresses of the broker and coSigner.
-     * @dev Require DEFAULT_ADMIN_ROLE to invoke. NOTE: once virtual addresses are set, there is no way to change them if their private key is lost. In such case, vault implementation contract becomes useless and requires an upgrade.
-     * @param brokerVirtualAddress Address derived from broker public key.
-     * @param coSignerVirtualAddress Address derived from coSigner public key.
-     */
-    function setup(address brokerVirtualAddress, address coSignerVirtualAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
-      require(
-        brokerVirtualAddress != address(0) && coSignerVirtualAddress != address(0),
-        'Invalid virtual address'
-      );
-      require(!isSetup, 'Vault is already setup');
-
-      isSetup = true;
-
-      _brokerVirtualAddress = brokerVirtualAddress;
-      _coSignerVirtualAddress = coSignerVirtualAddress;
-    }
-
-    /**
-     * @notice Get last ledger id (deposits and withdrawals id).
-     * @dev Get last ledger id (deposits and withdrawals id).
-     * @return uint256 Ledger id.
-     */
-    function getLastId() external view override returns (uint256) {
-        return _ledgerId.current();
-    }
-
-    /**
-     * @notice Set the address derived from the broker's new public key. Emits `BrokerVirtualAddressSet` event.
-     * @dev Supplied payload must be signed by broker's current public key.
-     * @param encodedAddress Encoded new virtual broker address.
-     * @param signature New virtual address signed by broker's current public key.
-     */
-    function setBrokerVirtualAddress(bytes memory encodedAddress, bytes calldata signature)
-        external
-    {
-        bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encodedAddress));
-        address recoveredSigner = ECDSA.recover(digest, signature);
-        require(recoveredSigner == _brokerVirtualAddress, 'Signer is not broker');
-
-        address newBrokerVirtualAddress = abi.decode(encodedAddress, (address));
-        require(newBrokerVirtualAddress != address(0),'Invalid virtual address');
-        _brokerVirtualAddress = newBrokerVirtualAddress;
-        emit BrokerVirtualAddressSet(newBrokerVirtualAddress);
-    }
-
-    /**
-     * @notice Set the address derived from the soSigner's new public key. Emits `CoSignerVirtualAddressSet` event.
-     * @dev Supplied payload must be signed by soSigner's current public key.
-     * @param encodedAddress Encoded new virtual soSigner address.
-     * @param signature New virtual address signed by soSigner's current public key.
-     */
-    function setCoSignerVirtualAddress(bytes memory encodedAddress, bytes calldata signature)
-        external
-    {
-        bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encodedAddress));
-        address recoveredSigner = ECDSA.recover(digest, signature);
-        require(recoveredSigner == _coSignerVirtualAddress, 'Signer is not coSigner');
-
-        address newCoSignerVirtualAddress = abi.decode(encodedAddress, (address));
-        require(newCoSignerVirtualAddress != address(0),'Invalid virtual address');
-        _coSignerVirtualAddress = newCoSignerVirtualAddress;
-        emit CoSignerVirtualAddressSet(newCoSignerVirtualAddress);
-    }
-
-    /**
-     * @notice Get broker virtual (only public key it is derived from exists) key for this vault.
-     * @dev Get broker virtual (only public key it is derived from exists) key for this vault.
-     * @return address Broker virtual (only public key it is derived from exists) key.
-     */
-    function getBrokerVirtualAddress() external view returns (address) {
-        return _brokerVirtualAddress;
-    }
-
-    /**
-     * @notice Get coSigner virtual (only public key it is derived from exists) key for this vault.
-     * @dev Get coSigner virtual (only public key it is derived from exists) key for this vault.
-     * @return address CoSigner virtual (only public key it is derived from exists) key.
-     */
-    function getCoSignerVirtualAddress() external view returns (address) {
-        return _coSignerVirtualAddress;
-    }
-
-    /**
-     * @notice Deposit assets with given payload from the caller. Emits `Deposited` event.
-     * @dev Deposit assets with given payload from the caller. Emits `Deposited` event.
-     * @param encodedPayload Encoded payload, which denotes action to be performed.
-     * @param brokerSignature Payload signed by the broker.
-     * @param coSignerSignature Payload signed by the coSigner.
-     */
-    function deposit(
-        bytes calldata encodedPayload,
-        bytes calldata brokerSignature,
-        bytes calldata coSignerSignature
-    ) external payable {
-        address issuer = msg.sender;
-
-        // check signatures
-        _requireSigNotUsed(issuer, brokerSignature);
-        _requireSigNotUsed(issuer, coSignerSignature);
-
-        _requireValidSignature(encodedPayload, _brokerVirtualAddress, brokerSignature);
-        _requireValidSignature(encodedPayload, _coSignerVirtualAddress, coSignerSignature);
-
-        // extract payload
-        Payload memory payload = abi.decode(encodedPayload, (Payload));
-
-        // check payload
-        _checkPayload(payload);
-
-        require(payload.action == DEPOSIT_ACTION, 'Invalid action');
-        require(payload.destination == issuer, 'Invalid destination');
-
-        // use signatures
-        _useSignature(issuer, brokerSignature);
-        _useSignature(issuer, coSignerSignature);
-
-        // deposit allocations
-        for (uint256 i = 0; i < payload.allocations.length; i++) {
-            _transferAssetFrom(
-                payload.allocations[i].asset,
-                payload.destination,
-                payload.allocations[i].amount
-            );
-
-            _ledgerId.increment();
-
-            emit Deposited(
-                _ledgerId.current(),
-                payload.destination,
-                payload.allocations[i].asset,
-                payload.allocations[i].amount,
-                payload.rid
-            );
-        }
-    }
-
-    /**
-     * @notice Withdraw assets with given payload to the destination specified in the payload. Emits `Withdrawn` event.
-     * @dev Withdraw assets with given payload to the destination specified in the payload. Emits `Withdrawn` event.
-     * @param encodedPayload Encoded payload, which denotes action to be performed.
-     * @param brokerSignature Payload signed by the Broker.
-     * @param coSignerSignature Payload signed by the coSigner.
-     */
-    function withdraw(
-        bytes calldata encodedPayload,
-        bytes calldata brokerSignature,
-        bytes calldata coSignerSignature
-    ) external payable {
-        address issuer = msg.sender;
-
-        // check signatures
-        _requireSigNotUsed(issuer, brokerSignature);
-        _requireSigNotUsed(issuer, coSignerSignature);
-
-        _requireValidSignature(encodedPayload, _brokerVirtualAddress, brokerSignature);
-        _requireValidSignature(encodedPayload, _coSignerVirtualAddress, coSignerSignature);
-
-        // extract payload
-        Payload memory payload = abi.decode(encodedPayload, (Payload));
-
-        // check payload
-        _checkPayload(payload);
-
-        require(payload.action == WITHDRAW_ACTION, 'Invalid action');
-
-        // use signatures
-        _useSignature(issuer, brokerSignature);
-        _useSignature(issuer, coSignerSignature);
-
-        // withdraw allocations
-        for (uint256 i = 0; i < payload.allocations.length; i++) {
-            _transferAssetTo(
-                payload.allocations[i].asset,
-                payload.destination,
-                payload.allocations[i].amount
-            );
-
-            _ledgerId.increment();
-
-            emit Withdrawn(
-                _ledgerId.current(),
-                payload.destination,
-                payload.allocations[i].asset,
-                payload.allocations[i].amount,
-                payload.rid
-            );
-        }
-    }
 
     /**
      * @notice Revert if hash of supplied signature was already used by the issuer.
@@ -340,5 +150,199 @@ contract VaultImpl is VaultImplBase, IVault {
         }
         /* solhint-disable no-inline-assembly */
         return id;
+    }
+
+    /**
+     * @notice The setup function sets virtual addresses of the broker and coSigner.
+     * @dev Require DEFAULT_ADMIN_ROLE to invoke. NOTE: once virtual addresses are set, there is no way to change them if their private key is lost. In such case, vault implementation contract becomes useless and requires an upgrade.
+     * @param brokerVirtualAddress Address derived from broker public key.
+     * @param coSignerVirtualAddress Address derived from coSigner public key.
+     */
+    function setup(address brokerVirtualAddress, address coSignerVirtualAddress)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(
+            brokerVirtualAddress != address(0) && coSignerVirtualAddress != address(0),
+            'Invalid virtual address'
+        );
+        require(!_isSetup, 'Vault is already setup');
+
+        _isSetup = true;
+
+        _brokerVirtualAddress = brokerVirtualAddress;
+        _coSignerVirtualAddress = coSignerVirtualAddress;
+    }
+
+    /**
+     * @notice Get last ledger id (deposits and withdrawals id).
+     * @dev Get last ledger id (deposits and withdrawals id).
+     * @return uint256 Ledger id.
+     */
+    function getLastId() external view override returns (uint256) {
+        return _ledgerId.current();
+    }
+
+    /**
+     * @notice Get broker virtual (only public key it is derived from exists) key for this vault.
+     * @dev Get broker virtual (only public key it is derived from exists) key for this vault.
+     * @return address Broker virtual (only public key it is derived from exists) key.
+     */
+    function getBrokerVirtualAddress() external view returns (address) {
+        return _brokerVirtualAddress;
+    }
+
+    /**
+     * @notice Set the address derived from the broker's new public key. Emits `BrokerVirtualAddressSet` event.
+     * @dev Supplied payload must be signed by broker's current public key.
+     * @param encodedAddress Encoded new virtual broker address.
+     * @param signature New virtual address signed by broker's current public key.
+     */
+    function setBrokerVirtualAddress(bytes memory encodedAddress, bytes calldata signature)
+        external
+    {
+        bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encodedAddress));
+        address recoveredSigner = ECDSA.recover(digest, signature);
+        require(recoveredSigner == _brokerVirtualAddress, 'Signer is not broker');
+
+        address newBrokerVirtualAddress = abi.decode(encodedAddress, (address));
+        require(newBrokerVirtualAddress != address(0), 'Invalid virtual address');
+        _brokerVirtualAddress = newBrokerVirtualAddress;
+        emit BrokerVirtualAddressSet(newBrokerVirtualAddress);
+    }
+
+    /**
+     * @notice Get coSigner virtual (only public key it is derived from exists) key for this vault.
+     * @dev Get coSigner virtual (only public key it is derived from exists) key for this vault.
+     * @return address CoSigner virtual (only public key it is derived from exists) key.
+     */
+    function getCoSignerVirtualAddress() external view returns (address) {
+        return _coSignerVirtualAddress;
+    }
+
+    /**
+     * @notice Set the address derived from the soSigner's new public key. Emits `CoSignerVirtualAddressSet` event.
+     * @dev Supplied payload must be signed by soSigner's current public key.
+     * @param encodedAddress Encoded new virtual soSigner address.
+     * @param signature New virtual address signed by soSigner's current public key.
+     */
+    function setCoSignerVirtualAddress(bytes memory encodedAddress, bytes calldata signature)
+        external
+    {
+        bytes32 digest = ECDSA.toEthSignedMessageHash(keccak256(encodedAddress));
+        address recoveredSigner = ECDSA.recover(digest, signature);
+        require(recoveredSigner == _coSignerVirtualAddress, 'Signer is not coSigner');
+
+        address newCoSignerVirtualAddress = abi.decode(encodedAddress, (address));
+        require(newCoSignerVirtualAddress != address(0), 'Invalid virtual address');
+        _coSignerVirtualAddress = newCoSignerVirtualAddress;
+        emit CoSignerVirtualAddressSet(newCoSignerVirtualAddress);
+    }
+
+    /**
+     * @notice Deposit assets with given payload from the caller. Emits `Deposited` event.
+     * @dev Deposit assets with given payload from the caller. Emits `Deposited` event.
+     * @param encodedPayload Encoded payload, which denotes action to be performed.
+     * @param brokerSignature Payload signed by the broker.
+     * @param coSignerSignature Payload signed by the coSigner.
+     */
+    function deposit(
+        bytes calldata encodedPayload,
+        bytes calldata brokerSignature,
+        bytes calldata coSignerSignature
+    ) external payable {
+        address issuer = msg.sender;
+
+        // check signatures
+        _requireSigNotUsed(issuer, brokerSignature);
+        _requireSigNotUsed(issuer, coSignerSignature);
+
+        _requireValidSignature(encodedPayload, _brokerVirtualAddress, brokerSignature);
+        _requireValidSignature(encodedPayload, _coSignerVirtualAddress, coSignerSignature);
+
+        // extract payload
+        Payload memory payload = abi.decode(encodedPayload, (Payload));
+
+        // check payload
+        _checkPayload(payload);
+
+        require(payload.action == DEPOSIT_ACTION, 'Invalid action');
+        require(payload.destination == issuer, 'Invalid destination');
+
+        // use signatures
+        _useSignature(issuer, brokerSignature);
+        _useSignature(issuer, coSignerSignature);
+
+        // deposit allocations
+        for (uint256 i = 0; i < payload.allocations.length; i++) {
+            _transferAssetFrom(
+                payload.allocations[i].asset,
+                payload.destination,
+                payload.allocations[i].amount
+            );
+
+            _ledgerId.increment();
+
+            emit Deposited(
+                _ledgerId.current(),
+                payload.destination,
+                payload.allocations[i].asset,
+                payload.allocations[i].amount,
+                payload.rid
+            );
+        }
+    }
+
+    /**
+     * @notice Withdraw assets with given payload to the destination specified in the payload. Emits `Withdrawn` event.
+     * @dev Withdraw assets with given payload to the destination specified in the payload. Emits `Withdrawn` event.
+     * @param encodedPayload Encoded payload, which denotes action to be performed.
+     * @param brokerSignature Payload signed by the Broker.
+     * @param coSignerSignature Payload signed by the coSigner.
+     */
+    function withdraw(
+        bytes calldata encodedPayload,
+        bytes calldata brokerSignature,
+        bytes calldata coSignerSignature
+    ) external payable {
+        address issuer = msg.sender;
+
+        // check signatures
+        _requireSigNotUsed(issuer, brokerSignature);
+        _requireSigNotUsed(issuer, coSignerSignature);
+
+        _requireValidSignature(encodedPayload, _brokerVirtualAddress, brokerSignature);
+        _requireValidSignature(encodedPayload, _coSignerVirtualAddress, coSignerSignature);
+
+        // extract payload
+        Payload memory payload = abi.decode(encodedPayload, (Payload));
+
+        // check payload
+        _checkPayload(payload);
+
+        require(payload.action == WITHDRAW_ACTION, 'Invalid action');
+
+        // use signatures
+        _useSignature(issuer, brokerSignature);
+        _useSignature(issuer, coSignerSignature);
+
+        // withdraw allocations
+        for (uint256 i = 0; i < payload.allocations.length; i++) {
+            _transferAssetTo(
+                payload.allocations[i].asset,
+                payload.destination,
+                payload.allocations[i].amount
+            );
+
+            _ledgerId.increment();
+
+            emit Withdrawn(
+                _ledgerId.current(),
+                payload.destination,
+                payload.allocations[i].asset,
+                payload.allocations[i].amount,
+                payload.rid
+            );
+        }
     }
 }
