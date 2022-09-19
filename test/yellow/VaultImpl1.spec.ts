88f7456d1c1454768e8ce1124abecbc95078e3b9
lckmidnight <lckmidnight@gmail.com>
2022-09-19 20:11:20 +0200

    drop BigNumber unused import in ImplVault tests

--- test/yellow/VaultImpl1.spec.ts
+++ test/yellow/VaultImpl1.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {BigNumber, Contract, providers, utils} from 'ethers';
+import {Contract, providers, utils} from 'ethers';
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 import {ethers} from 'hardhat';
 
