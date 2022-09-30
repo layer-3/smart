a3af7501eb5c79c36c92e439b955c15dc13a62dc
nksazonov <nsazonov@openware.com>
2022-09-30 09:49:41 +0300

    add tests layout

---
+++ test/yellow/NetworkRegistry.spec.ts
@@ -0,0 +1,48 @@
+import {expect} from 'chai';
+import {Contract, Wallet} from 'ethers';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {ethers} from 'hardhat';
+
+const AddressZero = ethers.constants.AddressZero;
+const ADM_ROLE = ethers.constants.HashZero;
+const MNTR_ROLE = ethers.utils.id('REGISTRY_MAINTAINER_ROLE');
+
+describe('Network Registry', () => {
+  describe('constructor', () => {
+    it('Revert on supplying not YellowClearingBase address');
+
+    it('Deployer is admin');
+  });
+
+  describe('participant data manipulation', () => {
+    it('Return true if participant is present');
+
+    it('Return false if participant is not present');
+
+    it('Return false if participant is present and with status "None"');
+
+    it('Return existing participant data');
+
+    it('Revert when getting unexisting participant data');
+
+    it('Maintainer can set participant data');
+
+    it('Revert on not maintainer setting participant data');
+
+    it('Revert on setting data of zero address');
+
+    it('Revert on setting data of migrated participant');
+  });
+
+  describe('migrate', () => {
+    it('Revert on migrate call to the first version');
+
+    it('Participant data is copied on migrate');
+
+    it('Participant is marked as migrated on migrate');
+
+    it('Revert on migrating unexisting participant');
+
+    it('Revert on migrating already migrated participant');
+  });
+});
