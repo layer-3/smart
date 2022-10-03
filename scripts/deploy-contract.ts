7894087d371b3cb2ce0dae16885c0d88be686081
Sazonov Nikita <35502225+nksazonov@users.noreply.github.com>
2022-10-03 11:22:09 +0300

    Refactor: networks, deploy script (#9)
    * add goerli, remove ropsten and rinkeby
    
    * add more logging to deploy script
    
    Co-authored-by: nksazonov <nsazonov@openware.com>

--- scripts/deploy-contract.ts
+++ scripts/deploy-contract.ts
@@ -1,18 +1,35 @@
 import {ethers} from 'hardhat';
 
 async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const args = process.env.CONTRACT_ARGS!.split(',').map((v) => v.trim());
-  console.log(`Args:`, args);
+  const provider = ethers.provider;
+  console.log('Current network:', provider.network.name);
+
+  const [deployer] = await ethers.getSigners();
+  console.log('Deployer address:', deployer.address);
+  console.log('Deployer balance:', (await deployer.getBalance()).toString());
+
+  let args;
+  if (process.env.CONTRACT_ARGS) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    args = process.env.CONTRACT_ARGS.split(',').map((v) => v.trim());
+    console.log(`Args:`, args);
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const factory = await ethers.getContractFactory(process.env.CONTRACT_FACTORY!);
-  const contract = await factory.deploy(...args);
+
+  let contract;
+  if (args) {
+    contract = await factory.deploy(...args);
+  } else {
+    contract = await factory.deploy();
+  }
+
   const {...deployTransaction} = contract.deployTransaction;
-  console.log('transaction:', deployTransaction);
+  console.log('Transaction:', deployTransaction);
   await contract.deployed();
 
-  console.log(`deployed to:`, contract.address);
+  console.log(`Deployed to:`, contract.address);
 }
 
 main().catch((error) => {
