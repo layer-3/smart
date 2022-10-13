862f22e5b3da6e3ebc12a440b566e6a449ac55e6
nksazonov <nsazonov@openware.com>
2022-10-13 11:05:58 +0300

    rename encodeSelf to signSelf

--- test/yellow/src/NetworkRegistry/transactions.ts
+++ test/yellow/src/NetworkRegistry/transactions.ts
@@ -1,15 +1,15 @@
 import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
 
-import {encodeSelf} from '../signatures';
+import {signSelf} from '../signatures';
 
 export type registerParams = [string, string];
 
 export async function registerParams(participant: SignerWithAddress): Promise<registerParams> {
-  return [participant.address, await encodeSelf(participant)];
+  return [participant.address, await signSelf(participant)];
 }
 
 export type migrateParams = [string, string];
 
 export async function migrateParams(participant: SignerWithAddress): Promise<migrateParams> {
-  return [participant.address, await encodeSelf(participant)];
+  return [participant.address, await signSelf(participant)];
 }
--- test/yellow/src/signatures.ts
+++ test/yellow/src/signatures.ts
@@ -4,7 +4,7 @@ export function signEncoded(signer: Signer, encodedData: string): Promise<string
   return signer.signMessage(utils.arrayify(utils.keccak256(encodedData)));
 }
 
-export async function encodeSelf(participant: Signer): Promise<string> {
+export async function signSelf(participant: Signer): Promise<string> {
   return await signEncoded(
     participant,
     utils.defaultAbiCoder.encode(['address'], [await participant.getAddress()])
