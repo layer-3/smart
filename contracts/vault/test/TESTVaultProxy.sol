5d8985c3e2f1b0fe394127cc153f6843cefbdbdd
nksazonov <nsazonov@openware.com>
2022-10-14 17:45:56 +0300

    fix constructor argument type

--- contracts/vault/test/TESTVaultProxy.sol
+++ contracts/vault/test/TESTVaultProxy.sol
@@ -2,12 +2,13 @@
 pragma solidity 0.8.16;
 
 import '../VaultProxyBase.sol';
+import '../VaultImplBase.sol';
 
 /**
  * @dev Use for TEST PURPOSES ONLY. !!! Contains security vulnerability !!!
  */
 contract TESTVaultProxy is VaultProxyBase {
-    constructor(address startImplementation) VaultProxyBase(startImplementation) {}
+    constructor(VaultImplBase startImplementation) VaultProxyBase(startImplementation) {}
     // SECURITY VULNERABILITY HERE ^^^
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
