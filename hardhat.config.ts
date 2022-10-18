a3c5530ce87287c4b0941d423497f349d01f4e15
nksazonov <nsazonov@openware.com>
2022-10-18 12:53:32 +0300

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,9 +20,6 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
   }
 });
 
-const ACCOUNTS = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? '';
-
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
