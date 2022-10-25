296262666f6609a2beb0026a2f9981f932c62f86
nksazonov <nsazonov@openware.com>
2022-10-25 12:29:02 +0300

    fix: renounced admin role from gnosis -> deployer Closes #27.

--- scripts/clearing/deploy-clearing-renounce-roles.ts
+++ scripts/clearing/deploy-clearing-renounce-roles.ts
@@ -43,7 +43,7 @@ async function main(): Promise<void> {
   console.log(`Renounced 'REGISTRY_MAINTAINER_ROLE' from deployer (${deployer.address})`);
 
   await Clearing.renounceRole(ADM_ROLE, deployer.address);
-  console.log(`Renounced 'DEFAULT_ADMIN_ROLE' from gnosis (${deployer.address})`);
+  console.log(`Renounced 'DEFAULT_ADMIN_ROLE' from deployer (${deployer.address})`);
 }
 
 main().catch((error) => {
