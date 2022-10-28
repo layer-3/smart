1255ecf8f9dc2fd5cefec50ab19868ccafd6db89
nksazonov <nsazonov@openware.com>
2022-10-28 16:14:23 +0300

    temporarily remove helpers.ts

--- docs/api/templates/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'node:fs';
-
-export function importUsage(contractName: string): string {
-  const path = `./docs/api/templates/usage/${contractName}.md`;
-  return readFileSync(path).toString();
-}
