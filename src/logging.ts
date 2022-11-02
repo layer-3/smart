e0bbbe9a7ca7898a9811d4bb7a70d358045aad6b
nksazonov <nsazonov@openware.com>
2022-11-02 17:42:30 +0200

    basic estimation & logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,9 +1,10 @@
 import { ethers } from 'hardhat';
+import { Contract, ContractFactory, utils } from 'ethers';
 
 import { GAS_TRACKER_SUPPORTED_NETWORKS, deploymentFees, transactionFees } from './gas-estimation';
-import { getNetworkName } from './networks';
+import { getNetworkExplorerURL, getNetworkName } from './networks';
 
-import type { Contract, ContractFactory } from 'ethers';
+export const SEPARATOR = '\n-------------------\n';
 
 interface LogOptions {
   network?: boolean;
@@ -33,6 +34,8 @@ export async function logEnvironment(lo: LogOptions = defaultLogOptions): Promis
     const balanceBigNum = await deployer.getBalance();
     console.log('Deployer balance:', balanceBigNum.toString());
   }
+
+  console.log(SEPARATOR);
 }
 
 async function logGasFees(estimated: number): Promise<void> {
@@ -42,7 +45,15 @@ async function logGasFees(estimated: number): Promise<void> {
     noteMsg = `(rough estimation, 60% of gasAmount * maxFeePerGas)`;
   }
 
-  console.log(`Estimated to consume ${estimated} gas`, noteMsg);
+  console.log(
+    `Estimated to consume ${utils.commify(estimated)} wei (${utils.commify(
+      utils.formatUnits(estimated, 'gwei'),
+    )} gwei)`,
+  );
+
+  if (noteMsg !== '') {
+    console.log(noteMsg);
+  }
 }
 
 export async function estimateAndLogTransactionFees(
@@ -62,4 +73,70 @@ export async function estimateAndLogDeploymentFees(
 ): Promise<void> {
   const estimated = await deploymentFees(factory, args);
   await logGasFees(estimated);
+}
+
+// message, hash or address
+type MessageAndHashOrAddress = [string, string];
+
+function isHashOrAddress(hashOrAddress: string): 'tx' | 'address' {
+  if (!ethers.utils.isHexString(hashOrAddress)) {
+    throw new Error('Not a hex string');
+  }
+
+  switch (hashOrAddress.length) {
+    case 42: {
+      return 'address';
+    }
+
+    case 66: {
+      return 'tx';
+    }
+
+    default: {
+      throw new Error('Not a hash or address');
+    }
+  }
+}
+
+async function _getExplorerURL(): Promise<string> {
+  let explorerURL = '';
+
+  try {
+    explorerURL = getNetworkExplorerURL(await getNetworkName());
+  } catch (error) {
+    if ((error as Error).cause !== 404) {
+      throw error;
+    }
+  }
+
+  return explorerURL;
+}
+
+function _logTxHashOrAddress(msgAndHshOrAddr: MessageAndHashOrAddress, explorerURL: string): void {
+  console.log(msgAndHshOrAddr[0], msgAndHshOrAddr[1]);
+
+  if (explorerURL !== '') {
+    const hshOrAddr = isHashOrAddress(msgAndHshOrAddr[1]);
+    console.log(`(${explorerURL}/${hshOrAddr}/${msgAndHshOrAddr[1]})`);
+  }
+
+  console.log();
+}
+
+export async function logTxHashOrAddress(
+  messageAndHashOrAddress: MessageAndHashOrAddress,
+): Promise<void> {
+  const explorerURL = await _getExplorerURL();
+
+  _logTxHashOrAddress(messageAndHashOrAddress, explorerURL);
+}
+
+export async function logTxHashesOrAddresses(
+  messagesAndHashesOrAddresses: MessageAndHashOrAddress[],
+): Promise<void> {
+  const explorerURL = await _getExplorerURL();
+
+  for (const msgAndHshOrAddr of messagesAndHashesOrAddresses) {
+    _logTxHashOrAddress(msgAndHshOrAddr, explorerURL);
+  }
 }
